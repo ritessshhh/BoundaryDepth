77c0a5771d248d0f3fc3efc92f7518d634b91a2d
R I T E S H <81812754+ritessshhh@users.noreply.github.com>
2024-04-05 02:19:32 -0400

    Saves vertices and camera pose as json

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from DepthEstimator import BoundaryDepthExtractor
 
 if __name__ == "__main__":
     boundaryDepthExtractor = BoundaryDepthExtractor()
-    # image_path = 'images/emptyRoom.png'
-    # boundaryDepthExtractor.createOBJ(image_path, 'output/')
+    image_path = 'images/emptyRoom.png'
+    boundaryDepthExtractor.createOBJ(image_path, 'output/')
     points = boundaryDepthExtractor.verticalPlaneExtraction("output/mesh.pcd")
     points_2d = boundaryDepthExtractor.orthogonalProjection(points)
     hull_points = boundaryDepthExtractor.boundaryDelineation(points_2d)
